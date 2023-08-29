eefb6bfcb5d1cd1e7961973215a265cbea71c7bb
stefan poterasu <spoterasu@gmail.com>
2023-08-29 22:22:24 +0300

    Version 1.0 Release

--- Autoclicker v 1.0/Form1.Designer.cs
+++ Autoclicker v 1.0/Form1.Designer.cs
@@ -28,13 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SettingsPanel = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Home_ClickMode_Label = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.LeftClickSwitch = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.RightClickSwitch = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SettingsPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(189, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "5";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(25, 185);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 37);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Interval (ms)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(257, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "✔";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // SettingsPanel
+            // 
+            this.SettingsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SettingsPanel.Controls.Add(this.RightClickSwitch);
+            this.SettingsPanel.Controls.Add(this.label6);
+            this.SettingsPanel.Controls.Add(this.LeftClickSwitch);
+            this.SettingsPanel.Controls.Add(this.label5);
+            this.SettingsPanel.Controls.Add(this.button1);
+            this.SettingsPanel.Controls.Add(this.checkBox1);
+            this.SettingsPanel.Controls.Add(this.textBox1);
+            this.SettingsPanel.Controls.Add(this.label1);
+            this.SettingsPanel.Controls.Add(this.label2);
+            this.SettingsPanel.Location = new System.Drawing.Point(12, 12);
+            this.SettingsPanel.Name = "SettingsPanel";
+            this.SettingsPanel.Size = new System.Drawing.Size(315, 426);
+            this.SettingsPanel.TabIndex = 7;
+            this.SettingsPanel.Visible = false;
+            this.SettingsPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.SettingsPanel_Paint);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Silver;
+            this.label5.Location = new System.Drawing.Point(62, 399);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Autoclicker made by Poterasu Stefan";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 394);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 24);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Down_Arrow";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-1, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "⚙︎ Settings ⚙︎";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stop Hotkey:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Start Hotkey:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(167, 357);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 24);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Up_Arrow";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Home_ClickMode_Label
+            // 
+            this.Home_ClickMode_Label.AutoSize = true;
+            this.Home_ClickMode_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Home_ClickMode_Label.Location = new System.Drawing.Point(79, 23);
+            this.Home_ClickMode_Label.Name = "Home_ClickMode_Label";
+            this.Home_ClickMode_Label.Size = new System.Drawing.Size(167, 25);
+            this.Home_ClickMode_Label.TabIndex = 7;
+            this.Home_ClickMode_Label.Text = "Mode: Left Click";
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.Home_ClickMode_Label);
+            this.MainPanel.Controls.Add(this.pictureBox1);
+            this.MainPanel.Controls.Add(this.button4);
+            this.MainPanel.Controls.Add(this.label3);
+            this.MainPanel.Controls.Add(this.button3);
+            this.MainPanel.Controls.Add(this.label4);
+            this.MainPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(315, 426);
+            this.MainPanel.TabIndex = 10;
+            // 
+            // LeftClickSwitch
+            // 
+            this.LeftClickSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeftClickSwitch.Location = new System.Drawing.Point(120, 146);
+            this.LeftClickSwitch.Name = "LeftClickSwitch";
+            this.LeftClickSwitch.Size = new System.Drawing.Size(83, 33);
+            this.LeftClickSwitch.TabIndex = 11;
+            this.LeftClickSwitch.Text = "Left Click";
+            this.LeftClickSwitch.UseVisualStyleBackColor = true;
+            this.LeftClickSwitch.Click += new System.EventHandler(this.LeftClickSwitch_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 33);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Mode:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // RightClickSwitch
+            // 
+            this.RightClickSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RightClickSwitch.Location = new System.Drawing.Point(209, 146);
+            this.RightClickSwitch.Name = "RightClickSwitch";
+            this.RightClickSwitch.Size = new System.Drawing.Size(83, 33);
+            this.RightClickSwitch.TabIndex = 13;
+            this.RightClickSwitch.Text = "Right Click";
+            this.RightClickSwitch.UseVisualStyleBackColor = true;
+            this.RightClickSwitch.Click += new System.EventHandler(this.RightClickSwitch_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Autoclicker_v_1._0.Properties.Resources.mouse_01_generated;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 351);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackgroundImage = global::Autoclicker_v_1._0.Properties.Resources.icon;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(333, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 45);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(409, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.SettingsPanel);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Autoclicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SettingsPanel.ResumeLayout(false);
+            this.SettingsPanel.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel SettingsPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Home_ClickMode_Label;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button RightClickSwitch;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button LeftClickSwitch;
     }
 }
 
--- Autoclicker v 1.0/Form1.cs
+++ Autoclicker v 1.0/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,181 @@ namespace Autoclicker_v_1._0
 {
     public partial class Form1 : Form
     {
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int LEFTUP = 0x0004;
+        private const int LEFTDOWN = 0X0002;
+        private const int RIGHTUP = 0x0010;
+        private const int RIGHTDOWN = 0x0008;
+        private int ACTIVEUP = 0x0004;
+        private int ACTIVEDOWN = 0X0002;
+        public int interval = 5;
+        public bool Click = false;
+        public int parsedValue;
+        private Keys selectedStartHotkey = Keys.Up;
+        private Keys selectedStopHotkey = Keys.Down;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            this.KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread AC = new Thread(Autoclick);
+            backgroundWorker1.RunWorkerAsync();
+            AC.Start();
+            SettingsPanel.Visible = false;
+            checkBox1.Checked = true;
+        }
+        private void Autoclick()
+        {
+            while (true)
+            {
+                if (Click)
+                {
+                    mouse_event(dwFlags: ACTIVEDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                    Thread.Sleep(interval);  // Wait for the click interval
+                    mouse_event(dwFlags: ACTIVEUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                }
+                Thread.Sleep(5);  // A small delay before the next iteration
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    if (GetAsyncKeyState(selectedStopHotkey) < 0)
+                    {
+                        Click = false;
+                    }
+                    else if (GetAsyncKeyState(selectedStartHotkey) < 0)
+                    {
+                        Click = true;
+                    }
+                    Thread.Sleep(1);
+                }
+                Thread.Sleep(1);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out parsedValue))
+            {
+                MessageBox.Show("Add a number");
+                return;
+            }
+            else
+            {
+                interval = int.Parse(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (SettingsPanel.Visible == true)
+            {
+                MainPanel.Visible = true;
+                SettingsPanel.Visible = false;
+            }
+            else 
+            {
+                SettingsPanel.Visible = true;
+                MainPanel.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // Subscribe to the KeyDown event of the form to capture the next key press
+            this.KeyDown += Form1_KeyDownAfterButton3Click;
+
+            // Focus the form to make sure it receives key events
+            this.Focus();
+
+        }
+
+        private void Form1_KeyDownAfterButton3Click(object sender, KeyEventArgs e)
+        {
+            // Update the button text with the pressed key's representation
+            button3.Text = $" {e.KeyCode}";
+            selectedStartHotkey = e.KeyCode;
+
+            // Unsubscribe from the KeyDown event
+            this.KeyDown -= Form1_KeyDownAfterButton3Click;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Subscribe to the KeyDown event of the form to capture the next key press
+            this.KeyDown += Form1_KeyDownAfterButton4Click;
+
+            // Focus the form to make sure it receives key events
+            this.Focus();
+        }
+
+        private void Form1_KeyDownAfterButton4Click(object sender, KeyEventArgs e)
+        {
+            // Update the button text with the pressed key's representation
+            button4.Text = $" {e.KeyCode}";
+            selectedStopHotkey = e.KeyCode;
+
+            // Unsubscribe from the KeyDown event
+            this.KeyDown -= Form1_KeyDownAfterButton4Click;
+        }
+
+        private void SettingsPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LeftClickSwitch_Click(object sender, EventArgs e)
+        {
+            ACTIVEDOWN = LEFTDOWN;
+            ACTIVEUP = LEFTDOWN;
+            pictureBox1.Image = Properties.Resources.mouse_01_generated;
+            Home_ClickMode_Label.Text = "Mode: Left Click";
+        }
+
+        private void RightClickSwitch_Click(object sender, EventArgs e)
+        {
+            ACTIVEDOWN = RIGHTDOWN;
+            ACTIVEUP = RIGHTDOWN;
+            pictureBox1.Image = Properties.Resources.mouse_02_generated;
+            Home_ClickMode_Label.Text = "Mode: Right Click";
         }
     }
 }
